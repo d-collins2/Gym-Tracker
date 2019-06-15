19475bea9335aed22642866d576bf608dd058da7
Damon Collins <damon.c.p.collins@gmail.com>
2019-06-14 22:51:20 -0400

    added stop watch component

--- src/components/stopwatch/StopWatch.js
+++ src/components/stopwatch/StopWatch.js
@@ -1,34 +1,79 @@
 import React from 'react';
-import Timer from './Timer.js';
-import TimerInput from './TimerInput.js';
-import Button from './Button.js';
+import StopwatchDisplay from './StopwatchDisplay.js';
 import './StopWatch.css';
 
 class StopWatch extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
+
     this.state = {
-      seconds: '00',   // responsible for the seconds
-      minutes: '0'  // responsible for the minutes
+      running: false,
+      currentTimeMs: 0,
+      currentTimeSec: 0,
+      currentTimeMin: 0,
+    };
+  }
+
+  formatTime = (val, ...rest) => {
+    let value = val.toString();
+    if (value.length < 2) {
+      value = '0' + value;
     }
-    // bind the method to the constructor
-    this.handleChange = this.handleChange.bind(this);
-  }
+    if (rest[0] === 'ms' && value.length < 3) {
+      value = '0' + value;
+    }
+    return value;
+  };
 
-  handleChange = (event) => {
+  start = () => {
+    if (!this.state.running) {
+      this.setState({ running: true });
+      this.watch = setInterval(() => this.pace(), 10);
+    }
+  };
+
+  stop = () => {
+    this.setState({ running: false });
+    clearInterval(this.watch);
+  };
+
+  pace = () => {
+    this.setState({ currentTimeMs: this.state.currentTimeMs + 10 });
+    if (this.state.currentTimeMs >= 1000) {
+      this.setState({ currentTimeSec: this.state.currentTimeSec + 1 });
+      this.setState({ currentTimeMs: 0 });
+    }
+    if (this.state.currentTimeSec >= 60) {
+      this.setState({ currentTimeMin: this.state.currentTimeMin + 1 });
+      this.setState({ currentTimeSec: 0 });
+    }
+  };
+
+  reset = () => {
     this.setState({
-      minutes: event.target.value
-    })
-  }
+      currentTimeMs: 0,
+      currentTimeSec: 0,
+      currentTimeMin: 0,
+    });
+  };
 
-  render(){
-    console.log(this.state)
-    const { minutes, seconds } = this.state
+  render() {
     return (
-      <div className="card">
-        <TimerInput minutes={minutes} handleChange={this.handleChange}/>
-        <Timer minutes={minutes} seconds={seconds}/>
-        <Button />
+      <div className={'stopwatch'}>
+        <h2 ref="header">Stopwatch</h2>
+        {this.state.running === false && (
+          <button onClick={this.start}>START</button>
+        )}
+        {this.state.running === true && (
+          <button onClick={this.stop}>STOP</button>
+        )}
+        <button onClick={this.reset}>RESET</button>
+        <StopwatchDisplay
+          ref="display"
+          {...this.state}
+          formatTime={this.formatTime}
+        />
+
       </div>
     );
   }
